5382a121b68eb5ed0ad2e4c47c6524da37544772
Walid Youssef <49271416+Night-Spider@users.noreply.github.com>
2023-05-24 01:22:51 +0300

    some bug fixes for the same coding challenge but not quite solved yet. will redo it at a later date

--- findBall5i.js
+++ findBall5i.js
@@ -6,10 +6,20 @@ function findBall(scales, ball_count) {
   let generated = [];
   for (let b = 0; b < ball_count; b++) generated.push(b);
   let groups = splitBalls(generated);
-  console.log(groups);
   let weighAttemptsLeft = getExponent(ball_count);
   let unweighedGroup;
   while (weighAttemptsLeft > 0) {
+    console.log(groups);
+    if (groups.length === 1 && unweighedGroup) {
+      let close = scales.getWeight(groups, unweighedGroup[0]);
+      if (close === 0) {
+        groups = unweighedGroup;
+        unweighedGroup = null;
+      } else if (close > 0) {
+        groups = unweighedGroup;
+        unweighedGroup = null;
+      } else if (close < 0) return groups[0];
+    } else if (groups.length === 1 && !unweighedGroup) return groups[0];
     const balance = scales.getWeight(groups[0], groups[1]);
     if (groups.length === 3) {
       if (balance === 1) {
@@ -30,18 +40,21 @@ function findBall(scales, ball_count) {
       }
     } else if (groups.length === 2) {
       if (balance === 1) {
-        if (groups[1].length === 1) return groups[1][0];
+        if (groups[1].length === 1) return groups[1];
         else {
           groups = splitBalls(groups[1]);
           weighAttemptsLeft--;
         }
       } else if (balance === -1) {
-        if (groups[0].length === 1) return groups[0][0];
+        if (groups[0].length === 1) return groups[0];
         else {
           groups = splitBalls(groups[0]);
           weighAttemptsLeft--;
         }
-      } else return (groups = unweighedGroup);
+      } else {
+        groups = unweighedGroup;
+        unweighedGroup = null;
+      }
     } else {
       unweighedGroup = groups[2];
       if (balance === 1) {
@@ -64,9 +77,10 @@ function findBall(scales, ball_count) {
   }
 }
 
-function getExponent(group) {
+function getExponent(groupLength) {
   let scaleUsesRange = {
-    2: [0, 9],
+    1: [0, 3],
+    2: [4, 9],
     3: [10, 27],
     4: [28, 81],
     5: [82, 243],
@@ -74,7 +88,10 @@ function getExponent(group) {
   };
   let exponent;
   for (const num in scaleUsesRange) {
-    if (group >= scaleUsesRange[num][0] && group <= scaleUsesRange[num][1]) {
+    if (
+      groupLength >= scaleUsesRange[num][0] &&
+      groupLength <= scaleUsesRange[num][1]
+    ) {
       exponent = Number(num);
       break;
     }
@@ -84,42 +101,55 @@ function getExponent(group) {
 
 function splitBalls(group) {
   const exponent = getExponent(group.length);
+  console.log(`exponent: ${exponent}`);
   const fixedGroup = group.slice(0, 3 ** (exponent - 1));
+  console.log(`fixedGroup, length: ${fixedGroup}, ${fixedGroup.length}`);
   const subDivision = group.slice(0, 3 ** (exponent - 2));
-  const variableGroup = group.length - fixedGroup.length;
-  if (variableGroup.length > fixedGroup.length)
+  const variableGroupLength = group.length - fixedGroup.length;
+  console.log(`variableGroupLength: ${variableGroupLength}`);
+  if (group.length === 1) return group;
+  else if (variableGroupLength > fixedGroup.length)
     return [
       fixedGroup,
       group.slice(3 ** (exponent - 1), 3 ** (exponent - 1) * 2),
       group.slice(3 ** (exponent - 1) * 2),
     ];
-  else if (variableGroup.length === fixedGroup.length)
+  else if (variableGroupLength === fixedGroup.length) {
+    console.log("y");
     return [fixedGroup, group.slice(3 ** (exponent - 1))];
-  else {
+  } else {
     return [
       subDivision,
       group.slice(3 ** (exponent - 2), 3 ** (exponent - 2) * 2),
-      group.slice(3 ** (exponent - 2) * 2, 3 ** (exponent - 2) * 3),
-      group.slice(3 ** (exponent - 2) * 3),
+      group.slice(3 ** (exponent - 2) * 2),
     ];
   }
 }
 
 // FAILED TEST CASES THUS FAR:
 // ================================================
-// Guessing out of 32 balls in 4 or fewer moves:
+// Guessing out of 16 balls in 3 or fewer moves:
 // Log
-// [ [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ],
-//   [ 9, 10, 11, 12, 13, 14, 15, 16, 17 ],
-//   [ 18, 19, 20, 21, 22, 23, 24, 25, 26 ],
-//   [ 27, 28, 29, 30, 31 ] ]
-// Expected: 2, instead got: undefined
-// ------------------------------------------------
-// Guessing out of 26 balls in 3 or fewer moves:
-// Log
+// exponent: 3
+// fixedGroup, length: 0,1,2,3,4,5,6,7,8, 9
+// variableGroupLength: 7
 // [ [ 0, 1, 2 ],
 //   [ 3, 4, 5 ],
-//   [ 6, 7, 8 ],
-//   [ 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ] ]
-// Expected: 7, instead got: undefined
+//   [ 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ] ]
+// exponent: 3
+// fixedGroup, length: 6,7,8,9,10,11,12,13,14, 9
+// variableGroupLength: 1
+// [ [ 6, 7, 8 ], [ 9, 10, 11 ], [ 12, 13, 14, 15 ] ]
+// exponent: 2
+// fixedGroup, length: 12,13,14, 3
+// variableGroupLength: 1
+// [ [ 12 ], [ 13 ], [ 14, 15 ] ]
+// exponent: 1
+// fixedGroup, length: 14, 1
+// variableGroupLength: 1
+// y
+// Expected: 15, instead got: undefined
+// Completed in 1ms
+// ------------------------------------------------
+
 // ------------------------------------------------
